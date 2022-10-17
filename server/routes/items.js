6ff2256b1f2e391130010dcf98bbe0d4ec40bf4b
khabizat <habizat22@gmail.com>
2022-10-17 16:54:10 -0400

    adjust db query

--- server/routes/items.js
+++ server/routes/items.js
@@ -3,19 +3,22 @@ const router = require("express").Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(
-      `SELECT items.id as id,
+      `SELECT 
+      items.id as id,
       items.name as name,
       items.price as price,
-      items.url as url
+      items.url as url,
+      categories.name as category_name
       FROM items
       JOIN categories
-      ON categories.id = items.category_id`
+      ON categories.id = items.category_id
+      ORDER BY items.price DESC;`
       ).then((response) => {
-      return res.json(response.rows);
-    }).catch((err) => {
-      console.log(err);
-      return res.json(err);
-    });
+        res.json(response.rows);
+      }).catch((err) => {
+        console.log(err);
+        return res.json(err);
+      });
   });
 
   return router;
