68310d481803f7e2c2bf852d343db766ba8a23c5
khabizat <habizat22@gmail.com>
2022-11-11 23:58:19 -0500

    add endpoint

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -11,5 +11,32 @@ module.exports = (db) => {
         res.json(response.rows);
     });
   });
+
+
+  router.get("/:categoryId", (req, res) => {
+    const { categoryId } = req.params;
+
+    const queryString = `
+    SELECT
+    items.id as item,
+    items.url as url
+    items.price as price,
+    items.category_id as category_id ,
+    category.name as name,
+    FROM items 
+    JOIN categories 
+    ON items.category_id = categories.id
+    WHERE categories.id = $1`;
+
+    db.query(queryString, [categoryId])
+      .then((response) => {
+        return res.json(response.rows);
+      })
+      .catch((err) => {
+        console.log("query ERROR");
+        return res.json(err);
+      });
+  });
+
   return router;
 };
