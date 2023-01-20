b051b8ceaf0335f07c652a3aafc6a57976bb6206
khabizat <habizat22@gmail.com>
2023-01-19 23:53:30 -0500

    Add buttons

--- client/src/components/CategoryListItem.js
+++ client/src/components/CategoryListItem.js
@@ -11,13 +11,22 @@ export default function CategoryListItem(props) {
       <li onClick={()=>props.setCategoryId(categoryId)}>
           <h2 className="flex p-8 text-base font-normal bg-yellow-100 text-gray-900 transition duration-75 hover:bg-yellow-500 dark:hover:bg-gray-700 dark:text-white">
             {props.name}
+            <button>
+              <svg 
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6">
+                <path 
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
           </h2>
       </li>
-      {/* <img
-            className="flex justify-start"
-            src="images/add.png"
-            alt="Add"
-          /> */}
     </aside>
   );
 }
