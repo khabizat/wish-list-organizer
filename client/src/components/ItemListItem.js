26a1a43201d0ea0df38bc47e10c544f0d890e8ad
khabizat <habizat22@gmail.com>
2023-02-11 22:21:02 -0500

    add delete icon

--- client/src/components/ItemListItem.js
+++ client/src/components/ItemListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Microlink from '@microlink/react'
+import Microlink from '@microlink/react';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 export default function ItemListItem(props) {
 
@@ -12,7 +13,10 @@ export default function ItemListItem(props) {
 
   return (
     <div className="p-5 hover:bg-blue-200 hover:border-blue-300 h-64 w-64 md:h-auto md:w-auto">
-      <span className="mt-4 flex justify-start">{name}</span>
+      <div className="flex justify-between">
+        <span className="mt-4 flex justify-start">{name}</span>
+        <DeleteOutlineOutlinedIcon/>
+      </div>
       <hr className="border-b-4 border-black"></hr>
       <span className="mt-4 flex justify-start">{price}</span>
       <Microlink url={url} />
