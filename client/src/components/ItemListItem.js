df385ad9cafac43227bdbc23d20415644d17d6d9
khabizat <habizat22@gmail.com>
2023-02-16 19:55:41 -0500

    show pre-populated form on edit button click

--- client/src/components/ItemListItem.js
+++ client/src/components/ItemListItem.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import Microlink from '@microlink/react';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+// import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import ItemForm from "./ItemForm";
+
 
 export default function ItemListItem(props) {
 
@@ -11,18 +16,45 @@ export default function ItemListItem(props) {
     onDelete
   } = props;
 
-  // console.log(props);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  }
+
+  const handleCancel = () => {
+    setIsEditing(false);
+  }
+  
+
+  if (isEditing) {
+    return (
+      <ItemForm
+        name={name}
+        url={url}
+        price={price}
+        onCancel={handleCancel}
+      />
+    );
+  }
+
   return (
     <div className="p-5 hover:bg-blue-200 hover:border-blue-300 h-64 w-64 md:h-auto md:w-auto">
       <div className="flex justify-between">
         <span className="mt-4 flex justify-start">{name}</span>
-        <button onClick={onDelete}>
-          <DeleteOutlineOutlinedIcon />
-        </button>
+        <div className="flex justify-end space-x-2">
+          <button onClick={handleEdit}>
+            <EditOutlinedIcon />
+          </button>
+          <button onClick={onDelete}>
+            <DeleteOutlineOutlinedIcon />
+          </button>
+        </div>
       </div>
       <hr className="border-b-4 border-black"></hr>
       <span className="mt-4 flex justify-start">{price}</span>
       <Microlink url={url} />
     </div>
-  )  
+  );
 }
