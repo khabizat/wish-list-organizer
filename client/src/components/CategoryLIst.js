ca4fac84af39f2330f171a0a47f2547292c537f2
khabizat <habizat22@gmail.com>
2023-02-25 23:14:17 -0500

    add MUI icons to the categories

--- client/src/components/CategoryLIst.js
+++ client/src/components/CategoryLIst.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import List from '@mui/material/List';
-// import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import CategoryListItem from "./CategoryListItem";
+
+import CheckroomOutlinedIcon from '@mui/icons-material/CheckroomOutlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+// import WatchOutlinedIcon from '@mui/icons-material/WatchOutlined';
+import ColorizeOutlinedIcon from '@mui/icons-material/ColorizeOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import SpaOutlinedIcon from '@mui/icons-material/SpaOutlined';
+import SmartphoneOutlinedIcon from '@mui/icons-material/SmartphoneOutlined';
+import ChairOutlined from "@mui/icons-material/ChairOutlined";
+import BlenderOutlinedIcon from '@mui/icons-material/BlenderOutlined';
+import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
+
+const categoryIcons = {
+  "Clothing & Shoes": <CheckroomOutlinedIcon />,
+  "Accessories": < BusinessCenterOutlinedIcon />,
+  // HiOutlineShoppingBag
+  "Skincare": <ColorizeOutlinedIcon />,
+  "Beauty": <StarBorderOutlinedIcon />,
+  "Wellness": < SpaOutlinedIcon />,
+  "Electronics": <SmartphoneOutlinedIcon />,
+  "Home Decor": <ChairOutlined />,
+  "Kitchen": <BlenderOutlinedIcon/>,
+  "Books": <AutoStoriesOutlinedIcon />,
+};
 
 export default function CategoryList(props) {
   const [categories, setCategories] = useState([]);
@@ -22,6 +45,7 @@ export default function CategoryList(props) {
     <ListItemButton  key={category.id}>
       <CategoryListItem
         name={category.name}
+        icon={categoryIcons[category.name]}
         categoryId={category.id}
         setCategoryId={props.onChange}
       />
--- client/src/components/CategoryListItem.js
+++ client/src/components/CategoryListItem.js
@@ -1,6 +1,4 @@
 import { React } from "react";
-// import Fab  from '@mui/material/Fab';
-// import AddIcon from '@mui/icons-material/Add';
 
 export default function CategoryListItem(props) {
 
@@ -9,11 +7,9 @@ export default function CategoryListItem(props) {
   return(
     <aside className="w-64">
       <li onClick={()=>props.setCategoryId(categoryId)}>
-          <h2 className="flex justify-between p-8 text-lg font-normal bg-yellow-100 text-gray-900 transition duration-75 hover:bg-yellow-500 dark:hover:bg-gray-700 dark:text-white">
+          <h2 className="flex justify-start gap-2 p-8 text-lg font-normal bg-yellow-100 text-gray-900 transition duration-75 hover:bg-yellow-500 dark:hover:bg-gray-700 dark:text-white">
+            {props.icon}
             {props.name}
-            {/* <Fab onClick={()=>alert(`You clicked ${props.name}`)} size="small" color="secondary" aria-label="add" >
-              <AddIcon />
-            </Fab> */}
           </h2>
       </li>
     </aside>
