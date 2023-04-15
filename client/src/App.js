f196011f7bfa084d656a2472e2301e9b997a0453
khabizat <habizat22@gmail.com>
2023-04-14 23:49:15 -0400

    add placeholder for sort feature

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CategoryList from "./components/CategoryList";
 import ItemForm from "./components/ItemForm";
 import ItemList from "./components/ItemList";
+import SortForm from "./components/SortForm";
 import useApplicationData from "./hooks/useApplicationData";
 // import useVisualMode from "./hooks/useVisualMode";
 
@@ -26,6 +27,10 @@ export default function App(props) {
       <div className="bg-white h-screen overflow-y-scroll w-2/5">
         <ItemList
         categoryId={state.categoryId}
+        />
+      </div>
+      <div className="bg-white h-screen overflow-y-scroll w-1/5">
+        <SortForm
         />
       </div>
     </main>
---
+++ client/src/components/SortForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SortForm(props) {
+
+  return (
+    <>
+      <h>Sort features</h>
+    </>
+  )
+
+}
